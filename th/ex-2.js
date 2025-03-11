642cbb0af1c9a2b14cf07a2e47420994140bd017
Binwaran <binwaran.m@gmail.com>
2025-03-11 11:45:53 +0800

    Add in class exercise

--- th/ex-2.js
+++ th/ex-2.js
@@ -13,6 +13,23 @@
 */
 
 // Start coding here
+function updateStudentScore(studentsInfo,targetStudent,addScore){
+    let left = 0;
+    let right = studentsInfo.length-1;
+
+    while (left<=right){
+        let mid = Math.floor((left+right)/2);
+        if (studentsInfo[mid].name === targetStudent){
+            studentsInfo[mid].score += addScore;
+            return `${targetStudent} score is now ${studentsInfo[mid].score}`;
+        } else if(studentsInfo[mid].name <targetStudent){
+            left = mid + 1; 
+        } else {
+            right = mid -1;
+        }
+    }
+    return `Not found ${targetStudent} on the list`;
+}
 
 const studentsInfo = [
   { name: "Alice", age: 14, classNumber: 8, score: 70 },
@@ -21,6 +38,7 @@ const studentsInfo = [
   { name: "Diana", age: 14, classNumber: 8, score: 75 },
   { name: "Ethan", age: 13, classNumber: 7, score: 47 },
 ];
+
 
 console.log(updateStudentScore(studentsInfo, "Ethan", 20));
 // Ethan score is now 67
